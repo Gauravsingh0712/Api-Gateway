ff8a80ec7848c90c861b468973f60bb84c1f0b00
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-12-26 22:40:49 +0530

    added isauthenticated middleware

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -3,6 +3,7 @@ const { UserRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
 const { Auth } = require('../utils/common');
 const userRepo = new UserRepository();
+
 async function create(data) {
     try {
         const user = await userRepo.create(data);
@@ -40,7 +41,29 @@ async function signin(data) {
     }
 }
 
+async function isAuthenticated(token) {
+    try {
+        if (!token) {
+            throw new AppError('Missing JWT token', StatusCodes.BAD_REQUEST);
+        }
+        const response = Auth.verifyToken(token);
+        const user = await userRepo.get(response.id);
+        if (!user) {
+            throw new AppError('No user found', StatusCodes.NOT_FOUND);
+        }
+        return user.id;
+    } catch (error) {
+        if (error instanceof AppError) throw error;
+        if (error.name == 'JsonWebTokenError') {
+            throw new AppError('Invalid JWT token', StatusCodes.BAD_REQUEST);
+        }
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
 module.exports = {
     create,
-    signin
+    signin,
+    isAuthenticated
 }
